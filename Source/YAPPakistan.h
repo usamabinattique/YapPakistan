c998922dc6c07649ad2b3b5b11d8a5cc78088ffa
Tayyab Akram <tayyab.akram@digitify.com>
2021-08-09 14:25:45 +0500

    Added project for YAP Pakistan

---
+++ Source/YAPPakistan.h
@@ -0,0 +1,18 @@
+//
+//  YAPPakistan.h
+//  YAPPakistan
+//
+//  Created by Tayyab on 09/08/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for YAPPakistan.
+FOUNDATION_EXPORT double YAPPakistanVersionNumber;
+
+//! Project version string for YAPPakistan.
+FOUNDATION_EXPORT const unsigned char YAPPakistanVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YAPPakistan/PublicHeader.h>
+
+
