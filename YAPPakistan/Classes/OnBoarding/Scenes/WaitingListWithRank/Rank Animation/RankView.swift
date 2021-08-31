7708d07ca6118354a9ca5c30658bd0b8f770d729
Tayyab Akram <tayyab.akram@digitify.com>
2021-08-31 19:05:38 +0500

    Code Refactoring

--- YAPPakistan/Classes/OnBoarding/Scenes/WaitingListWithRank/Rank Animation/RankView.swift
+++ YAPPakistan/Classes/OnBoarding/Scenes/WaitingListWithRank/Rank Animation/RankView.swift
@@ -11,90 +11,93 @@ import UIKit
 import YAPComponents
 
 public class RankView: UIView {
-    
     private lazy var hStack: UIStackView = UIStackViewFactory.createStackView(with: .horizontal, alignment: .center, distribution: .fill, spacing: 14)
-    
+
     // MARK: Initialization
-    
+
     public override init(frame: CGRect) {
         super.init(frame: frame)
         commonInit()
     }
-    
+
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         commonInit()
     }
-    
+
     private func commonInit() {
         setupViews()
         setupConstraints()
     }
-    
+
     public func animate(rank string: String) {
-        
         createViews(string)
     }
 }
 
 extension RankView {
-    
     func setupViews() {
         translatesAutoresizingMaskIntoConstraints = false
         clipsToBounds = true
         addSubview(hStack)
     }
-    
+
     func setupConstraints() {
         hStack
             .alignEdgesWithSuperview([.top, .bottom])
             .centerHorizontallyInSuperview()
     }
-    
+
     func createViews(_ string: String) {
-        let array = create7digitString(string)
-        var delay = 0.0
-        hStack.arrangedSubviews.forEach{
+        hStack.arrangedSubviews.forEach {
             $0.removeFromSuperview()
         }
-        
+
+        let array = create7digitString(string)
+        var delay = 0.0
+
         let minBoxSize = CGFloat(35.0)
-        let multiplier = (minBoxSize * CGFloat(array.count)) / 375
+        let multiplier = (minBoxSize * CGFloat(array.count)) / 375.0
         var boxWidth = (UIScreen.main.bounds.width * multiplier) / CGFloat(array.count)
         boxWidth = boxWidth > 50 ? 50 : boxWidth
-        
-        array.forEach{ string in
+
+        array.forEach { string in
             let view = RollingAnimationView()
             view
                 .width(constant: boxWidth)
                 .height(constant: 58)
+
             hStack.addArrangedSubview(view)
+
             let duration = 0.1
-            let labels = self.createLabels(string)
-            DispatchQueue.main.asyncAfter(deadline: .now() + delay) {  view.animate(labels: labels, singleAnimationDuration: duration, bounce: true ) }
-            delay += (duration * Double(labels.count/5))
+            let labels = createLabels(string)
+
+            DispatchQueue.main.asyncAfter(deadline: .now() + delay) {
+                view.animate(labels: labels, singleAnimationDuration: duration, bounce: true )
+            }
+
+            delay += (duration * Double(labels.count / 5))
         }
     }
 }
 
 extension RankView {
     func create7digitString(_ string: String) -> [String] {
-        
         if string.count >= 7 {
-            return string.map{String($0)}
+            return string.map { String($0) }
         }
-        return create7digitString("0"+string)
-       
+
+        return create7digitString("0" + string)
     }
     
     func createLabels(_ string: String) -> [String] {
         let number = (Int(string) ?? 0) - 1 /// add an extra value for bouncing effect
-        var array = [String]()
+        var array: [String] = []
         for i in number...9 {
-           
-            array.insert(String( i >= 0 ? i : 9), at: 0)
+            array.insert(String(i >= 0 ? i : 9), at: 0)
         }
         array.insert("0", at: 0)
+
         return array
     }
 }
--- YAPPakistan/Classes/OnBoarding/Scenes/WaitingListWithRank/Rank Animation/RollingAnimationView.swift
+++ YAPPakistan/Classes/OnBoarding/Scenes/WaitingListWithRank/Rank Animation/RollingAnimationView.swift
@@ -10,46 +10,41 @@ import Foundation
 import UIKit
 import YAPComponents
 
- public class RollingAnimationView : UIView {
-    lazy var label : UILabel = UIFactory.makeLabel(with: #colorLiteral(red: 0.1529999971, green: 0.1330000013, blue: 0.3840000033, alpha: 1), textStyle: .title2, alignment: .center, numberOfLines: 1)
-    
+public class RollingAnimationView: UIView {
+    private lazy var label: UILabel = UIFactory.makeLabel(with: #colorLiteral(red: 0.1529999971, green: 0.1330000013, blue: 0.3840000033, alpha: 1), textStyle: .title2, alignment: .center, numberOfLines: 1)
+
     // MARK: Initialization
-    
+
     public override init(frame: CGRect) {
         super.init(frame: frame)
         commonInit()
     }
-    
+
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         commonInit()
     }
-    
+
     private func commonInit() {
-        
         setupViews()
         setupConstraints()
-        render()
     }
-    
-    func render() {
-        self.layer.cornerRadius = 4
-        self.clipsToBounds = true
-    }
-    
-    func setupViews() {
+
+    private func setupViews() {
         translatesAutoresizingMaskIntoConstraints = false
-        self.addSubview(label)
-        self.backgroundColor = #colorLiteral(red: 0.9294117647, green: 0.9411764706, blue: 0.9725490196, alpha: 1)
+        backgroundColor = #colorLiteral(red: 0.9294117647, green: 0.9411764706, blue: 0.9725490196, alpha: 1)
+        clipsToBounds = true
+        layer.cornerRadius = 4
+
+        addSubview(label)
     }
-    
-    func setupConstraints() {
+
+    private func setupConstraints() {
         label
             .alignAllEdgesWithSuperview()
     }
-    
+
     public func animate(labels: [String], singleAnimationDuration: Double, bounce: Bool) {
-        
         label.animate(atIndex: 0, labels: labels, interval: singleAnimationDuration, bounce: bounce)
     }
 }
