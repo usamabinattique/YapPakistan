484bbc9984a4273a3346a786a6d8b6bfb9d3749c
Sarmad <sarmad.abbas@digitify.com>
2021-09-21 09:53:59 +0500

    loginOPTCoordinator

---
+++ YAPPakistan/Classes/Modules/LoginFlow/OPT/LoginOPTCoordinator.swift
@@ -0,0 +1,74 @@
+//
+//  LoginOPTCoordinator.swift
+//  YAPPakistan
+//
+//  Created by Sarmad on 21/09/2021.
+//
+
+import Foundation
+import RxSwift
+import YAPCore
+import UIKit
+
+enum LoginOPTVerificationResult {
+    case onboarding
+    case dashboard(session: Session)
+    case cancel
+}
+
+protocol LoginOPTCoordinatorType: Coordinator<LoginOPTVerificationResult> {
+
+    var root: UINavigationController! { get }
+    var container:YAPPakistanMainContainer! { get }
+    var result: PublishSubject<LoginOPTVerificationResult> { get }
+
+}
+
+class LoginOPTCoordinator: Coordinator<LoginOPTVerificationResult>, LoginOPTCoordinatorType {
+    
+    var root: UINavigationController!
+    var container: YAPPakistanMainContainer!
+    var result = PublishSubject<LoginOPTVerificationResult>()
+
+    init(root: UINavigationController,
+         xsrfToken: String,
+         container: YAPPakistanMainContainer
+    ){
+        self.root = root
+        self.container = container
+        self.container.xsrfToken = xsrfToken
+    }
+
+    override func start(with option: DeepLinkOptionType?) -> Observable<LoginOPTVerificationResult> {
+        
+        let otpMessage =  "screen_device_registration_otp_display_text_message".localized
+        let appLogo = UIImage(named: "icon_app_logo")
+        let authService = container.makeAuthorizationProvider(xsrfToken: container.xsrfToken)
+        let apiConfig = container.makeAPIConfiguration()
+        //let credentials = container.credentialsStore
+        let sessionCreator = UserSessionContainer(parent: container, session: Session(guestToken: "", sessionToken: ""))
+        
+        let otpRepository = OTPRepository(messageService: MessagesService(apiConfig: apiConfig, authorizationProvider: authService),
+                                          customerService: CustomersService(apiConfig: apiConfig, authorizationProvider: authService))
+        let viewModel = LoginOTPVerificationViewModel(action: .deviceVerification,
+                                                 heading: nil,
+                                                 subheading: NSAttributedString(string: otpMessage),
+                                                 image: appLogo,
+                                                 repository: otpRepository,
+                                                 username: "00923331699972", //credentials.username,
+                                                 passcode: "1212", //credentials.passcode,
+                                                 sessionCreator: sessionCreator as! SessionProviderType)
+        let viewController =  VerifyMobileOTPViewController(themeService: container.themeService, viewModel: viewModel)
+        
+        viewController.rx.viewDidAppear.withUnretained(self).subscribe(onNext: {
+            $0.0.result.onNext(.cancel)
+            $0.0.result.onCompleted()
+            $0.0.root.viewControllers.remove(at: $0.0.root.viewControllers.count - 2)
+        }).disposed(by: rx.disposeBag)
+        
+        root.pushViewController(viewController)
+
+        return result
+    }
+    
+}
--- YAPPakistan/Classes/Modules/LoginFlow/Passcode/VerifyPasscodeCoordinator.swift
+++ YAPPakistan/Classes/Modules/LoginFlow/Passcode/VerifyPasscodeCoordinator.swift
@@ -63,32 +63,10 @@ class PasscodeCoordinator: Coordinator<PasscodeVerificationResult>, PasscodeCoor
     
     func optVerification() {
         
-        let otpMessage =  "screen_device_registration_otp_display_text_message".localized
-        let appLogo = UIImage(named: "icon_app_logo")
-        let authService = container.makeAuthorizationProvider(xsrfToken: container.xsrfToken)
-        let apiConfig = container.makeAPIConfiguration()
-        //let credentials = container.credentialsStore
-        let sessionCreator = UserSessionContainer(parent: container, session: Session(guestToken: "", sessionToken: ""))
-        
-        let otpRepository = OTPRepository(messageService: MessagesService(apiConfig: apiConfig, authorizationProvider: authService),
-                                          customerService: CustomersService(apiConfig: apiConfig, authorizationProvider: authService))
-        let viewModel = LoginOTPVerificationViewModel(action: .deviceVerification,
-                                                 heading: nil,
-                                                 subheading: NSAttributedString(string: otpMessage),
-                                                 image: appLogo,
-                                                 repository: otpRepository,
-                                                 username: "00923331699972", //credentials.username,
-                                                 passcode: "1212", //credentials.passcode,
-                                                 sessionCreator: sessionCreator as! SessionProviderType)
-        let viewController =  VerifyMobileOTPViewController(themeService: container.themeService, viewModel: viewModel)
-        
-        viewController.rx.viewDidAppear.withUnretained(self).subscribe(onNext: {
-            $0.0.result.onNext(.cancel)
-            $0.0.result.onCompleted()
-            $0.0.root.viewControllers.remove(at: $0.0.root.viewControllers.count - 2)
-        }).disposed(by: rx.disposeBag)
-        
-        root.pushViewController(viewController)
+        coordinate(to: LoginOPTCoordinator(root: root, xsrfToken: container.xsrfToken, container: container))
+            .subscribe(onNext: { result in
+                print(result)
+            }).disposed(by: rx.disposeBag)
     }
     
 }
