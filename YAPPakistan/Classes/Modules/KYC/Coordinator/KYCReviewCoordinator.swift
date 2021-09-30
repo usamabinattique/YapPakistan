266e62620ccd49d85df48c17efaab99d52b8fd0e
Tayyab Akram <tayyab.akram@digitify.com>
2021-09-30 11:59:31 +0500

    Wrote coordinator for review screens

---
+++ YAPPakistan/Classes/Modules/KYC/Coordinator/KYCReviewCoordinator.swift
@@ -0,0 +1,67 @@
+//
+//  KYCReviewCoordinator.swift
+//  YAPPakistan
+//
+//  Created by Tayyab on 29/09/2021.
+//
+
+import Foundation
+import RxSwift
+import YAPCore
+
+class KYCReviewCoordinator : Coordinator<ResultType<Void>> {
+    private let container: UserSessionContainer
+    private let root: UINavigationController!
+    private let cnicOCR: CNICOCR
+
+    private let resultSubject = PublishSubject<ResultType<Void>>()
+    private let disposeBag = DisposeBag()
+
+    init(container: UserSessionContainer,
+         root: UINavigationController,
+         cnicOCR: CNICOCR) {
+        self.container = container
+        self.root = root
+        self.cnicOCR = cnicOCR
+    }
+
+    override func start(with option: DeepLinkOptionType?) -> Observable<ResultType<Void>> {
+        let reviewViewController = container.makeKYCInitialReviewViewController(cnicOCR: cnicOCR)
+        let reviewViewModel = reviewViewController.viewModel
+
+        reviewViewModel.outputs.rescan
+            .subscribe(onNext: { [weak self] in
+                self?.root.popViewController(animated: true)
+
+                self?.resultSubject.onNext(.cancel)
+                self?.resultSubject.onCompleted()
+            })
+            .disposed(by: disposeBag)
+
+        reviewViewModel.outputs.cnicInfo
+            .subscribe(onNext: { _ in
+                // FIXME: Perform navigation to full review.
+            })
+            .disposed(by: disposeBag)
+
+        let navigationController = UINavigationController(rootViewController: reviewViewController)
+        navigationController.navigationBar.isHidden = true
+
+        let progressViewController = container.makeKYCProgressViewController(navigationController: navigationController)
+        let progressViewModel: KYCProgressViewModelType! = progressViewController.viewModel
+        progressViewModel.inputs.progressObserver.onNext(0.25)
+
+        root.pushViewController(progressViewController, animated: true)
+
+        progressViewModel.outputs.backTap
+            .subscribe(onNext: { [weak self] in
+                self?.root.popViewController(animated: true)
+
+                self?.resultSubject.onNext(.cancel)
+                self?.resultSubject.onCompleted()
+            })
+            .disposed(by: disposeBag)
+
+        return resultSubject
+    }
+}
