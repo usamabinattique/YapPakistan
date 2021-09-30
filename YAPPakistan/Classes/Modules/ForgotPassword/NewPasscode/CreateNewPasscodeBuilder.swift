be2f439d2e2ea1b8fe0f7a969da5c34ac7048514
Sarmad <sarmad.abbas@digitify.com>
2021-09-30 18:03:12 +0500

    dependencies updates

--- YAPPakistan/Classes/Modules/ForgotPassword/NewPasscode/CreateNewPasscodeBuilder.swift
+++ YAPPakistan/Classes/Modules/ForgotPassword/NewPasscode/CreateNewPasscodeBuilder.swift
@@ -14,10 +14,22 @@ struct CreateNewPasscodeBuilder {
     func viewController() -> PasscodeViewController {
         let pinRepository = container.makePINRepository()
         let username = container.credentialsStore.getUsername() ?? ""
+
+        let strings = PasscodeViewStrings(
+            heading: "screen_create_passcode_display_text_title".localized,
+            agrement: "screen_create_passcode_display_text_terms_and_conditions".localized,
+            terms: "screen_create_passcode_display_button_terms_and_conditions".localized,
+            action: "screen_create_passcode_button_create_new_passcode".localized
+        )
+        let pinRange: ClosedRange<Int> = 4...6
+
         let viewModel = CreateNewPasscodeViewModel(repository: pinRepository,
                                                    credentialsManager: container.credentialsStore,
                                                    username: username,
-                                                   token: token)
+                                                   token: token,
+                                                   passcodeViewStrings: strings,
+                                                   pinRange: pinRange)
+        
         return PasscodeViewController(themeService: container.themeService, viewModel: viewModel)
     }
 }
