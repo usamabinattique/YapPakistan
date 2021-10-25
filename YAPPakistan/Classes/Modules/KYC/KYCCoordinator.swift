b17b9d90be8733e6af8280cd92bcb1254316a033
Sarmad <sarmad.abbas@digitify.com>
2021-10-25 15:17:08 +0500

    added address module with flow

--- YAPPakistan/Classes/Modules/KYC/KYCCoordinator.swift
+++ YAPPakistan/Classes/Modules/KYC/KYCCoordinator.swift
@@ -70,6 +70,11 @@ class KYCCoordinator: Coordinator<ResultType<Void>> {
             .subscribe(onNext: { $0.0.cardName() })
             .disposed(by: rx.disposeBag)
 
+        homeViewController.viewModel.outputs.next.filter({ $0 == .addressPending })
+            .withUnretained(self)
+            .subscribe(onNext: { $0.0.address() })
+            .disposed(by: rx.disposeBag)
+
         addChildVC(homeViewController)
     }
 
@@ -231,7 +236,7 @@ class KYCCoordinator: Coordinator<ResultType<Void>> {
                 self.setProgressViewHidden(true)
                 self.root.setNavigationBarHidden(true, animated: true)
                 self.root.setViewControllers(vcs, animated: true)
-                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {  }
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) { self.address() }
             })
             .disposed(by: rx.disposeBag)
 
@@ -257,6 +262,48 @@ class KYCCoordinator: Coordinator<ResultType<Void>> {
             .do(onNext: { [weak self] _ in self?.root.popViewController(animated: true) })
 
         return next
+    }
+
+    func address() {
+        let viewController = AddressViewController(themeService: container.themeService,
+                                                   viewModel: AddressViewModel())
+
+        viewController.viewModel.outputs.back.withUnretained(self)
+            .subscribe(onNext: { `self`, _ in
+                self.root.setNavigationBarHidden(true, animated: true)
+                self.root.popViewController(animated: true)
+            })
+            .disposed(by: rx.disposeBag)
+
+        let citySelected = viewController.viewModel.outputs.city.withUnretained(self)
+            .flatMap{ `self`, _ in self.selectCityName() }
+            .share()
+        citySelected.bind(to: viewController.viewModel.inputs.citySelectObserver)
+            .disposed(by: rx.disposeBag)
+        citySelected.subscribe(onNext: { [unowned self] _ in self.root.popViewController(animated: true) })
+            .disposed(by: rx.disposeBag)
+
+        root.pushViewController(viewController, animated: true)
+        root.setNavigationBarHidden(false, animated: true)
+    }
+
+    func selectCityName() -> Observable<String>  {
+
+        let viewModel = CountryViewModel(kycRepository: container.makeKYCRepository())
+        let viewController = CountrysViewController(themeService: container.themeService,
+                                                    viewModel: viewModel)
+
+        viewController.viewModel.outputs.back.withUnretained(self)
+            .subscribe(onNext: { `self`, _ in
+                self.root.setNavigationBarHidden(true, animated: true)
+                self.root.popViewController(animated: true)
+            })
+            .disposed(by: rx.disposeBag)
+
+        root.pushViewController(viewController, animated: true)
+        root.setNavigationBarHidden(false, animated: true)
+
+        return viewController.viewModel.outputs.next
     }
 
 }
