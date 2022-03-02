b921052dfeaaf5f89a983322888d77d699b82385
Zara M <>
2022-03-02 16:23:51 +0500

    logout event firing added, reverted scripts and fixed issue in super-app instead

--- YAPPakistan/Classes/Models/PhoneNumber.swift
+++ YAPPakistan/Classes/Models/PhoneNumber.swift
@@ -12,20 +12,18 @@ struct PhoneNumber {
     private(set) var number: String?
     private(set) var countryCode: String?
     private(set) var readableCountryCode: String?
-    private var pkDialCode: String { "0092" }
-    private var pkReadableDialCode: String { "+92" }
     
-//    public var formattedValue: String? {
-//        didSet {
-//            var components = formattedValue?.components(separatedBy: " ")
-//            readableCountryCode = components?.first
-//            countryCode = components?.first?.replacingOccurrences(of: "+", with: "00")
-//            components?.removeFirst()
-//            number = components?.joined()
-//            number = number?.replacingOccurrences(of: "-", with: "")
-//
-//        }
-//    }
+    public var formattedValue: String? {
+        didSet {
+            var components = formattedValue?.components(separatedBy: " ")
+            readableCountryCode = components?.first
+            countryCode = components?.first?.replacingOccurrences(of: "+", with: "00")
+            components?.removeFirst()
+            number = components?.joined()
+            number = number?.replacingOccurrences(of: "-", with: "")
+
+        }
+    }
 
     public var serverFormattedValue: String? {
         return (countryCode ?? "") + (number ?? "")
@@ -36,23 +34,13 @@ struct PhoneNumber {
     }
 
     public init(formattedValue: String?) {
-        //self.formattedValue = formattedValue
+        self.formattedValue = formattedValue
 
-            // var components = formattedValue?.components(separatedBy: " ")
-        guard var valueString = formattedValue else { return }
-        let isCode = valueString.contains(pkDialCode)
-        let isReadableCode = valueString.contains(pkReadableDialCode)
-        guard (isCode || isReadableCode) == true else { return }
-        if isCode {
-            countryCode = String(valueString.prefix(pkDialCode.length))
-            valueString = valueString.removingPrefix(pkDialCode)
-            readableCountryCode = countryCode?.replacingOccurrences(of: "00", with: "+") }
-        else {
-            readableCountryCode = String(valueString.prefix(pkReadableDialCode.length))
-            valueString = valueString.removingPrefix(pkReadableDialCode)
-            countryCode = readableCountryCode?.replacingOccurrences(of: "+", with: "00")
-        }
-        number = valueString
+        var components = formattedValue?.components(separatedBy: " ")
+        readableCountryCode = components?.first
+        countryCode = components?.first?.replacingOccurrences(of: "+", with: "00")
+        components?.removeFirst()
+        number = components?.joined()
         number = number?.replacingOccurrences(of: "-", with: "")
     }
 }
