6626cb8aad58f256004f2f1ed0271b3103f66610
Muhammad Umair <muhammad.umair@digitify.com>
2022-06-27 12:48:54 +0500

    Build number bumped to 79 Environment set to QA OCR api guardian name made optional

--- YAPPakistan/Classes/Modules/KYC/CP01_OCRReview/ReviewDetails/KYCReviewDetailsViewModel.swift
+++ YAPPakistan/Classes/Modules/KYC/CP01_OCRReview/ReviewDetails/KYCReviewDetailsViewModel.swift
@@ -82,7 +82,7 @@ class KYCReviewDetailsViewModel: KYCReviewDetailsViewModelInput, KYCReviewDetail
             KYCReviewFieldViewModel(heading: "screen_kyc_review_details_full_name".localized,
                                     value: self.cnicInfo.name, valueChanged: nameValueSubject.asObserver(), isEditable: true),
             KYCReviewFieldViewModel(heading: "screen_kyc_review_details_father_spouse_name".localized,
-                                    value: self.cnicOCR.guardianName, valueChanged: fatherNameValueSubject.asObserver(), isEditable: true),
+                                    value: self.cnicOCR.guardianName ?? "", valueChanged: fatherNameValueSubject.asObserver(), isEditable: true),
             KYCReviewFieldViewModel(heading: "screen_kyc_review_details_gender".localized,
                                     value: self.cnicInfo.gender),
             KYCReviewFieldViewModel(heading: "screen_kyc_review_details_dob".localized,
@@ -141,7 +141,7 @@ class KYCReviewDetailsViewModel: KYCReviewDetailsViewModelInput, KYCReviewDetail
 
                 return kycRepository.saveDocuments(documents, documentType: documentType,
                                                    identityNo: identityNo, nationality: nationality,
-                                                   fullName: fullName, fatherName: fatherName, gender: "\(gender.charactersArray[0])", dob: dob,
+                                                   fullName: fullName, fatherName: fatherName ?? "", gender: "\(gender.charactersArray[0])", dob: dob,
                                                    dateIssue: dateIssue, dateExpiry: dateExpiry)
             }
             .share()
